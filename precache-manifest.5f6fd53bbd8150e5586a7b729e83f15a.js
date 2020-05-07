0105de97ff93a862dab492601c97bd08640e3092
Bhavin2513 <bhavinpatel225.bp@gmail.com>
2020-05-07 20:08:45 +0530

    Updates

---
+++ precache-manifest.5f6fd53bbd8150e5586a7b729e83f15a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e9ff5e33c1ace8f8be7942eb803fa1b",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "70de4af745b1b0a12556",
+    "url": "/todo-app/static/css/main.d540cd92.chunk.css"
+  },
+  {
+    "revision": "2987e9a823f3ba2edc53",
+    "url": "/todo-app/static/js/2.a783e81c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-app/static/js/2.a783e81c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70de4af745b1b0a12556",
+    "url": "/todo-app/static/js/main.ff373cba.chunk.js"
+  },
+  {
+    "revision": "2986ea79fb7a68355c87",
+    "url": "/todo-app/static/js/runtime-main.a8e7edd0.js"
+  }
+]);
